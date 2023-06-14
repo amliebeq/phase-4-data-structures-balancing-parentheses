691eb81ea47405ca11ddfd01679024f904283185
alex lieberman <alex.lieberman182@gmail.com>
2023-06-14 10:20:55 -0700

    commit tree bb6f56f288cbd12d632da15c49af9a303f177e36

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,24 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(str)
+    Stack.new
+
+    left = []
+    right = []
+
+    str.chars.each do |char|
+        if char == '('
+            left << char
+        elsif char == ')'
+            right << char
+        end
+    end
+
+    total = left.length - right.length
+
+    if total < 0
+        total * -1
+    else
+        total
+    end
+end
